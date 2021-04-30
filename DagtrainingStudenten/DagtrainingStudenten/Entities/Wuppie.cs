31bc562fa04d5ef82911a6b34b0f43bd2ee1e788
JP <jp@jp.nl>
2021-04-30 09:46:21 +0200

    Eerste demo: basic databinding

---
+++ DagtrainingStudenten/DagtrainingStudenten/Entities/Wuppie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DagtrainingStudenten.Entities
+{
+    public class Wuppie
+    {
+		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Color { get; set; }
+
+		public string PhotoUrl { get; set; }
+	}
+}
---
+++ DagtrainingStudenten/DagtrainingStudenten/Pages/Basics.razor.cs
@@ -0,0 +1,44 @@
+﻿using DagtrainingStudenten.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DagtrainingStudenten.Pages
+{
+	public partial class Basics
+	{
+		List<Wuppie> Wuppies = new List<Wuppie>
+		{
+			new Wuppie
+			{
+				Id = 4,
+				Color = "Orange",
+				Name = "Wil-Alex",
+				PhotoUrl = "https://www.johan-laurens.nl/_oranjegoed/de09775.jpg"
+			},
+			new Wuppie
+			{
+				Id = 8,
+				Color = "Blue",
+				Name = "Maxima",
+				PhotoUrl = "https://www.dekattensite.nl/wp-content/uploads/2016/11/rl-wuppie.jpg?quality=100.3021031019220"
+			},
+			new Wuppie
+			{
+				Id = 15,
+				Color = "Green",
+				Name = "Alexia",
+				PhotoUrl = "https://www.zintuig.nl/images/supplier/222-/xl/bugs0632_a.jpg"
+			}
+		};
+
+		public string Naam { get; set; } = "JP";
+
+		void VeranderNaam()
+		{
+			Naam += "Sem";
+		}
+	}
+}
